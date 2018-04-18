912d94805e2943f56a94c4d852e4283b3421a14f
Przemyslaw Grygiel <pgrygiel@juniper.net>
2018-04-18 11:46:17 +0200

    create-lab.sh fix2

--- create-lab.sh
+++ create-lab.sh
@@ -4,7 +4,7 @@ sudo yum -y install qemu-kvm qemu-img virt-manager libvirt libvirt-python libvir
 sudo systemctl start libvirtd
 cd /tmp
 sudo wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
-sudo virt-customize --password ubuntu:password:contrail123 -a /tmp/trusty-server-cloudimg-amd64-disk1.img --run-command "apt-get -y purge cloud-init" --run-command "add-apt-repository ppa:cz.nic-labs/bird" --run-command  "apt-get update" --run-command "apt-get -y --force-yes install bird apache2" --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthenticationn yes/' /etc/ssh/sshd_config" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/bird.template > /root/bird.template" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/rc.local > /etc/rc.local"
+sudo virt-customize --root-password password:contrail123 --password ubuntu:password:contrail123 -a /tmp/trusty-server-cloudimg-amd64-disk1.img  --run-command "add-apt-repository ppa:cz.nic-labs/bird" --run-command  "apt-get update" --run-command "apt-get -y --force-yes install bird apache2" --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthenticationn yes/' /etc/ssh/sshd_config" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/bird.template > /root/bird.template" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/rc.local > /etc/rc.local"
 sudo systemctl stop libvirtd
 source  /etc/kolla/admin-openrc.sh 
 openstack image create --disk-format qcow2 --container-format bare --public --file /tmp/trusty-server-cloudimg-amd64-disk1.img ubuntu-lab
