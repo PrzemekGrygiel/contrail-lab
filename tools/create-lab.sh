c5f7419657c2ace720e9b492e9ada02dd30d40b9
Przemyslaw Grygiel <pgrygiel@juniper.net>
2018-04-22 14:25:41 +0200

    moving create lab scripts to tool dir

--- tools/create-lab.sh
+++ tools/create-lab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "starting create Openstck demo VM"
-sudo yum -y install qemu-kvm qemu-img virt-manager libvirt libvirt-python libvirt-client virt-install libguestfs-tools
+sudo yum -y install qemu-kvm qemu-img virt-manager libvirt libvirt-python libvirt-client virt-install libguestfs-tools wget
 yum install -y gcc python-devel wget
 pip install python-openstackclient
 pip install python-ironicclient
@@ -9,7 +9,7 @@ pip install python-heatclient
 sudo systemctl start libvirtd
 cd /tmp
 sudo wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
-sudo virt-customize --root-password password:contrail123 --password ubuntu:password:contrail123 -a /tmp/trusty-server-cloudimg-amd64-disk1.img  --run-command "add-apt-repository ppa:cz.nic-labs/bird" --run-command  "apt-get update" --run-command "apt-get -y --force-yes install bird apache2" --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthenticationn yes/' /etc/ssh/sshd_config" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/bird.template > /root/bird.template" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/rc.local > /etc/rc.local"
+sudo virt-customize --root-password password:contrail123 --password ubuntu:password:contrail123 -a /tmp/trusty-server-cloudimg-amd64-disk1.img  --run-command "add-apt-repository ppa:cz.nic-labs/bird" --run-command  "apt-get update" --run-command "apt-get -y --force-yes install bird apache2" --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthenticationn yes/' /etc/ssh/sshd_config" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/tools/bird.template > /root/bird.template" --run-command "curl https://raw.githubusercontent.com/PrzemekGrygiel/contrail-lab/master/tools/rc.local > /etc/rc.local"
 sudo systemctl stop libvirtd
 source  /etc/kolla/admin-openrc.sh 
 openstack image create --disk-format qcow2 --container-format bare --public --file /tmp/trusty-server-cloudimg-amd64-disk1.img ubuntu-lab
